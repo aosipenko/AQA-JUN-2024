6b93ca149c82b4c5bd56e178f6e6f9f7f4a259a7
alexkuzmenko48 <alexkyzmenko48@gmail.com>
2024-06-24 16:23:00 +0200

    Homework 1

--- src/main/java/org/prog/Main.java
+++ src/main/java/org/prog/Main.java
@@ -26,9 +26,9 @@ public class Main {
         car1.color = "Red";
         car2.color = "Blue";
 
-        car1.goTo();
+        car1.goTo(15);
         paintItBlack(car1);
-        car1.goTo();
+        car1.goTo(25);
     }
 
     public static void paintItBlack(Car carToPaint) {
