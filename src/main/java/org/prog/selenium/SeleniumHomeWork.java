1d7784150c4e80c07859cfd0a55fbe5a7294f3bb
DanyilMarkov <dmarkovson@gmail.com>
2024-08-04 13:20:37 +0300

    Hw 5

--- src/main/java/org/prog/selenium/SeleniumHomeWork.java
+++ src/main/java/org/prog/selenium/SeleniumHomeWork.java
@@ -1,5 +1,11 @@
 package org.prog.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumHomeWork {
 
     /**
@@ -13,5 +19,25 @@ public class SeleniumHomeWork {
 
     public static void main(String[] args) {
 
+        WebDriver driver = null;
+        try {
+
+            driver = new ChromeDriver();
+            driver.get("https://allo.ua/");
+
+            Thread.sleep(3000);
+
+            WebElement searchBox = driver.findElement (By.id("search-form__input"));;
+
+            searchBox.sendKeys("iphone 15");
+            searchBox.sendKeys(Keys.ENTER);
+
+            Thread.sleep(5000);
+
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
     }
 }
